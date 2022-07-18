3f9b0f67dfbd9dd23d131920c752196e003d9c3d
Calum <calumcmccall@gmail.com>
2022-07-18 12:05:12 +0100

    matches exist within leagues and can only be played against teams in that league

--- app/controllers/matches_controller.py
+++ app/controllers/matches_controller.py
@@ -17,15 +17,17 @@ def matches():
     return render_template("matches/index.html", matches=matches, teams=teams)
 
 # NEW
-@matches_blueprint.route("/matches/new")
-def new_match():
+@matches_blueprint.route("/matches/new/<id>")
+def new_match(id):
+    team_type = league_repository.select(id).team_type
+    clubs = team_repository.clubs_in_league(team_type)
     teams = team_repository.select_all()
-    leagues = league_repository.select_all()
-    return render_template("matches/new.html", teams=teams, leagues=leagues)
+    league = league_repository.select(id)
+    return render_template("matches/new.html", team_type=team_type, clubs=clubs, teams=teams, league=league)
 
 # CREATE
-@matches_blueprint.route("/matches", methods=["POST"])
-def create_match():
+@matches_blueprint.route("/matches/<id>", methods=["POST"])
+def create_match(id):
     home_team = team_repository.select(request.form["home_team"])
     home_team_for_match = MatchDayTeam(home_team.id)
     match_day_team_repository.save(home_team_for_match)
@@ -38,7 +40,7 @@ def create_match():
     home_team_score = request.form["home_team_score"]
     away_team_score = request.form["away_team_score"]
     
-    league = league_repository.select(request.form["league"])
+    league = league_repository.select(id)
     new_match = Match(home_team_for_match, away_team_for_match, home_team_score, away_team_score, league)
     
     match_repository.save(new_match)
